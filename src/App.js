05c23acdcd25a8598ad73c7a8c943a7c822d1eeb
buraklacinel <buraklacinel@gmail.com>
2021-11-24 16:20:53 +0300

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
+  const [showEvents, setShowEvents] = useState(true);
   const [events, setEvents] = useState([
     { title: "mario's birthday bash", id: 1 },
     { title: "bowser's live stream", id: 2 },
@@ -9,32 +10,36 @@ function App() {
   ]);
 
   const handleClick = (id) => {
-    // setEvents(
-    //   events.filter((event) => {
-    //     return id !== event.id;
-    //   })
-    // );
-
-    // Yukarıda "setEvents()" içerisinde "events" current state'ini kullanıyoruz."setEvents()" ile güncellediğimiz "events", güncelleme gerçekleşmeden önce değişime uğrayabilir. Bu beklenmedik hatalara sebep olabilir. Bunun için "setEvents()"e state'in o anki değerini dönen bir fonksiyon yazmalıyız.
     setEvents((prevEvents) => {
       return prevEvents.filter((event) => {
-        return id !== event.id;
+        return event.id !== id;
       });
     });
-
-    // Temel Kural; "state"i önceki değerine göre güncellememiz gerektiğinde, İlk olarak callback function ile önceki değere ulaşmak gerekir.
   };
 
   return (
     <div>
-      {events.map((event, index) => (
-        <div key={event.id}>
-          <h4>
-            {index + 1} - {event.title}
-          </h4>
-          <button onClick={() => handleClick(event.id)}>delete</button>
+      {showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(false)}>hide events</button>
         </div>
-      ))}
+      )}
+
+      {!showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(true)}>show events</button>
+        </div>
+      )}
+
+      {showEvents &&
+        events.map((event, index) => (
+          <div key={index}>
+            <h4>
+              {index + 1} - {event.title}
+            </h4>
+            <button onClick={() => handleClick(event.id)}>Delete</button>
+          </div>
+        ))}
     </div>
   );
 }
