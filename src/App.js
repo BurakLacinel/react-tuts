9022def797e5abe78ac3c317be1c39f777a9ad5c
buraklacinel <buraklacinel@gmail.com>
2021-11-25 12:50:08 +0300

    children props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+// Component'lara "prop" olarak "function"da verebiliriz. Bu genellikle; child component parent component'ın state'ini değiştirmesi gerektiğinde kullanılır.
+
 import "./App.css";
 import React, { useState } from "react";
 import Title from "./components/Title";
 import Modal from "./components/Modal";
 
 const App = () => {
+  const [showModal, setShowModal] = useState(true);
+
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
   return (
     <div className="App">
       <Title
@@ -11,21 +19,18 @@ const App = () => {
         subtitle="All the latest events in mario kingdom"
       />
 
-      {/* <Modal>
-        <h2>10% Off Coupon Code!!</h2>
-        <p>Use the code NINJA10 at the checkout.</p>
-      </Modal> */}
-
-      <Modal>
-        <h2>Terms and Conditions</h2>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis hic
-          autem sunt a doloribus quam recusandae vero, corrupti iure quasi
-          mollitia incidunt vel dolores. Laborum doloremque natus unde suscipit
-          excepturi.
-        </p>
-        <a href="#">find out more...</a>
-      </Modal>
+      {showModal && (
+        <Modal handleClose={handleClose}>
+          <h2>Terms and Conditions</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis
+            hic autem sunt a doloribus quam recusandae vero, corrupti iure quasi
+            mollitia incidunt vel dolores. Laborum doloremque natus unde
+            suscipit excepturi.
+          </p>
+          <a href="#">find out more...</a>
+        </Modal>
+      )}
     </div>
   );
 };
