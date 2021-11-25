9cf19b27d2198e76e69bbaece121444dfdf7963e
buraklacinel <buraklacinel@gmail.com>
2021-11-25 12:43:10 +0300

    intro to using multiple components

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,12 +1,14 @@
 // <React.Fragment> kullandığımızda "React"i kesin import etmeliyiz.
 // <React.Fragment>'ın key prop'ı yoksa <> </> şeklinde kullanabiliriz.
 
+import "./Title.css";
+
 export default function Title({ title, subtitle }) {
   return (
-    <>
+    <div className="title-block">
       <h1 className="title">{title}</h1>
       <br />
       <h2 className="subtitle">{subtitle}</h2>
-    </>
+    </div>
   );
 }
